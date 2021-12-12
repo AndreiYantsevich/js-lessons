e445aa755c0e18036e01205b2b0253c418f0998c
Andrei <yantsevich92@gmail.com>
2021-12-12 17:14:39 +0300

    lesson3

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = 'ce8674ff';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance.get(`${configOMB.baseURL}/?apikey=${key}&t=${title}`)
     },
     searchFilmsByType: (title: string, type: string) => {
+        return axiosInstance.get(`${configOMB.baseURL}/?apikey=${key}&t=${title}&type=${type}`)
     }
 };
 
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,4 +1,4 @@
-console.log('lesson 3');
+import axios from 'axios';
 
 // Event loop
 // https://learn.javascript.ru/event-loop
@@ -14,6 +14,23 @@ console.log('lesson 3');
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
+
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+    title: 'Andrei',
+    body: 'Andrei body',
+    userId: 1
+}).then(res => console.log(res.data))
+
+axios.get('https://jsonplaceholder.typicode.com/posts').then(res => console.log(res.data))
+
+axios.put('https://jsonplaceholder.typicode.com/posts/1', {
+    id: 1,
+    title: 'Andrei Yan',
+    body: 'Body ne body',
+    userId: 1
+}).then(res => console.log(res.data))
+
+axios.delete('https://jsonplaceholder.typicode.com/posts/1').then(res => console.log(res.data))
 
 
 // just a plug
